a8693af31053270f85773f2128bb821e148096a4
ChetanaBehere <chetanabehere821@gmail.com>
2024-12-15 14:08:11 +0530

    Update Project2.js

--- JAVASCRIPT/Project2.js
+++ JAVASCRIPT/Project2.js
@@ -1,13 +1,1 @@
-const inputBox  =document.getElementById("input-box");
-const listcontainer =document.getElementById("list-container");
-function addTask(){
-    if(inputBox.value === ''){
-        alert("You Must Write Something!");
-    }
-    else{
-        let li=document.createElement("li");
-        li.innerHTML=inputBox.value;
-        listcontainer.appendChild(li);
-    }
-    inputBox.value='';
-}
+
